1cbd9210527286d9f4f1187e3d64f6fe9e67dbee
SAINT YT <benjaminogunyinka12@gmail.com>
2025-09-03 15:46:33 +0100

    Update config.js

--- config.js
+++ config.js
@@ -27,7 +27,7 @@ ANTI_LINK: process.env.ANTI_LINK || "true",
 // make anti link true,false for groups 
 MENTION_REPLY: process.env.MENTION_REPLY || "false",
 // make true if want auto voice reply if someone menetion you 
-MENU_IMAGE_URL: process.env.MENU_IMAGE_URL || "https://files.catbox.moe/8lxlpq.jpg",
+MENU_IMAGE_URL: process.env.MENU_IMAGE_URL || "https://img1.pixhost.to/images/8429/637342945_uploaded_image.jpg",
 // add custom menu and mention reply image url
 PREFIX: process.env.PREFIX || ".",
 // add your prefix for bot   
@@ -47,7 +47,7 @@ OWNER_NAME: process.env.OWNER_NAME || "*χ SAINT*",
 // add bot owner name
 DESCRIPTION: process.env.DESCRIPTION || "*© ᴘᴏᴡᴇʀᴇᴅ ʙʏ χ SAINT ❣️*",
 // add bot owner name    
-ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/8lxlpq.jpg",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://img1.pixhost.to/images/8429/637342945_uploaded_image.jpg",
 // add img for alive msg
 LIVE_MSG: process.env.LIVE_MSG || "> SAINT X ⚡",
 // add alive msg here 
