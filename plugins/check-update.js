f6f5ba338fc62bc1cf97dcf1ca528611c063bd7b
SAINT YT <benjaminogunyinka12@gmail.com>
2025-09-05 00:40:54 +0100

    Update check-update.js

--- plugins/check-update.js
+++ plugins/check-update.js
@@ -16,7 +16,7 @@ cmd({
     if (!isOwner) return reply("This command is only for the bot owner.");
 
     try {
-        await reply("🔍 Checking for HASI-MD updates...");
+        await reply("🔍 Checking for SAINT-MD updates...");
 
         // Fetch the latest commit hash from GitHub
         const { data: commitData } = await axios.get("https://api.github.com/repos/chhaseeb47/HASI-MD/commits/main");
@@ -26,10 +26,10 @@ cmd({
         const currentHash = await getCommitHash();
 
         if (latestCommitHash === currentHash) {
-            return reply("✅ Your HASI-MD bot is already up-to-date!");
+            return reply("✅ Your SAINT-MD bot is already up-to-date!");
         }
 
-        await reply("🚀 Updating HASI-MD Bot...");
+        await reply("🚀 Updating SAINT-MD Bot...");
 
         // Download the latest code
         const zipPath = path.join(__dirname, "latest.zip");
